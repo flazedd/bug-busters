61d2280541661b9f9cfd216816054ce1a17c2f8a
Reinier <reinier7175@gmail.com>
2024-05-18 14:18:23 +0200

    exec mutpy from python

--- TESTTESTTEST.py
+++ TESTTESTTEST.py
@@ -1,3 +1,22 @@
+import subprocess
 import os
 
-os.execvp('mut.py', ['-h'])
+
+def exec_mutpy(folder, file_name, test_name):
+    target = f'PythonPUT/{folder}/{file_name}.py'
+    unit_test = f'PythonPUT/{folder}/{test_name}.py'
+    mut_script_path = os.path.join('.', 'venv', 'Scripts', 'mut.py')
+    # print(mut_script_path)
+    # # Run the script using subprocess
+    result = subprocess.run(['python', mut_script_path,
+                             '--target', target,
+                             '--unit-test', unit_test,
+                             '--runner', 'pytest'],
+                            capture_output=True, text=True, shell=True)
+    # Print the output
+    # print("Standard Output:")
+    # print(result.stdout)
+    result = result.stdout
+    return result
+
+print(exec_mutpy('codetiming', '_timers', 'test__timers'))
--- classes/python_implementation.py
+++ classes/python_implementation.py
@@ -69,9 +69,17 @@ class PythonImplementation(LanguageImplementation):
     def exec_mutpy(folder, file_name, test_name):
         target = f'PythonPUT/{folder}/{file_name}.py'
         unit_test = f'PythonPUT/{folder}/{test_name}.py'
-
-        result = subprocess.run(["mut.py", "--target", target, '--unit-test', unit_test, '--runner pytest'], shell=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE, text=True)
+        mut_script_path = os.path.join('.', 'venv', 'Scripts', 'mut.py')
+        # print(mut_script_path)
+        # # Run the script using subprocess
+        result = subprocess.run(['python', mut_script_path,
+                                 '--target', target,
+                                 '--unit-test', unit_test,
+                                 '--runner', 'pytest'],
+                                capture_output=True, text=True, shell=True)
+        # Print the output
+        # print("Standard Output:")
+        # print(result.stdout)
         result = result.stdout
         return result
 
